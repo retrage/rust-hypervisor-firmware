e422443f866ddc6e21701566b00e362700eb0111
Akira Moroo <retrage01@gmail.com>
2020-10-26 10:11:45 +0900

    [WIP] Change to use AHCI by default
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/efi/mod.rs
+++ src/efi/mod.rs
@@ -625,7 +625,7 @@ pub fn efi_exec(
     loaded_size: u64,
     info: &dyn boot::Info,
     fs: &crate::fat::Filesystem,
-    block: *const crate::virtioblk::VirtioBlockDevice,
+    block: *const crate::ahci::AhciIoPort,
 ) {
     let mut rs = efi::RuntimeServices {
         hdr: efi::TableHeader {
