db8beef911b5142b0282f46e6146ae92d2507600
Akira Moroo <retrage01@gmail.com>
2021-03-16 15:55:11 +0900

    efi: Use C representation for FileInfo
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/efi/file.rs
+++ src/efi/file.rs
@@ -182,7 +182,7 @@ pub extern "win64" fn set_position(_: *mut FileProtocol, _: u64) -> Status {
     Status::UNSUPPORTED
 }
 
-#[allow(unused)]
+#[repr(C)]
 struct FileInfo {
     size: u64,
     file_size: u64,
