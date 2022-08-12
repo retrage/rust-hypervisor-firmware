4e305cb0774c4dd90839f749f16cefe2257b2d92
Akira Moroo <retrage01@gmail.com>
2022-08-12 15:38:55 +0900

    [WIP] main: Remove unused PCI-related code
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -196,40 +196,6 @@ pub extern "C" fn rust64_start() -> ! {
     main()
 }
 
-/*
-const MAX_DEVICES: u8 = 32;
-const MAX_FUNCTIONS: u8 = 8;
-
-const INVALID_VENDOR_ID: u16 = 0xffff;
-
-struct PciConfig {
-    region: mem::MemoryRegion,
-}
-
-impl PciConfig {
-    const fn new(base: usize, size: usize) -> Self {
-        Self { region: mem::MemoryRegion::new(base as u64, size as u64) }
-    }
-
-    fn read(&self, bus: u8, device: u8, func: u8, offset: u8) -> (u64, u32) {
-        // dbg!(bus, device, func, offset);
-        assert_eq!(offset % 4, 0);
-        assert!(device < MAX_DEVICES);
-        assert!(func < MAX_FUNCTIONS);
-
-        // dev[bus][device][function]
-        let addr = PciConfig::get_addr(bus as u64, device as u64, func as u64);
-
-        (addr, self.region.io_read_u32(addr + u64::from(offset & 0xfc)))
-    }
-
-    fn get_addr(bus: u64, device: u64, func: u64) -> u64 {
-        const ECAM_SIZE: u64 = 0x1000;
-        ECAM_SIZE * (func + device * MAX_FUNCTIONS as u64 + bus * (MAX_DEVICES as u64 * MAX_FUNCTIONS as u64))
-    }
-}
-*/
-
 fn main() -> ! {
     log!("\nBooting...");
 
@@ -249,48 +215,6 @@ fn main() -> ! {
         log!("Device ID: {:#x}", device_id);
         log!("Vendor ID: {:#x}", vendor_id);
         virt_mmio_base += 0x200;
-    }
-    */
-    //const PCI_MMIO_BASE: usize = 0x3EFF0000;
-    //const PCI_MMIO_BASE: usize = 0x4010000000;
-    //const PCI_MMIO_SIZE: usize = 0x10000000;
-    // const PCI_MMIO_BASE: usize = 0x10000000;
-    // const PCI_MMIO_BASE: usize = 0x8000000000;
-    /*
-    let pci_config = PciConfig::new(PCI_MMIO_BASE, PCI_MMIO_SIZE);
-
-    for bus in 0..=u8::MAX {
-        for device in 0..MAX_DEVICES {
-            let (addr, data) = pci_config.read(bus, device, 0, 0);
-            let (vendor_id, device_id) = ((data & 0xffff) as u16, (data >> 16) as u16);
-            if vendor_id == INVALID_VENDOR_ID {
-                continue;
-            }
-            log!(
-                "Found PCI device vendor={:#04x} device={:#04x} in [{:02x}:{:02x}] at {:#x}",
-                vendor_id,
-                device_id,
-                bus,
-                device,
-                addr
-            );
-        }
-    }
-    */
-
-    /*
-    let pci_mmio = mem::MemoryRegion::new(PCI_MMIO_BASE as u64, PCI_MMIO_SIZE as u64);
-    pci_mmio.io_write_u8(0x12, 0x00);
-    pci_mmio.io_write_u8(0x12, 0x01);
-    pci_mmio.io_write_u8(0x12, 0x03);
-    let value_32 = pci_mmio.io_read_u32(0x00);
-    //let vendor_id = pci_mmio.io_read_u16(0x00);
-    //let device_id = pci_mmio.io_read_u16(0x02);
-    //log!("{:#x}:{:#x}", vendor_id, device_id);
-    log!("value_32: {:#x}", value_32);
-    for offset in 0x14..=0x1a {
-        let value_u8 = pci_mmio.io_read_u8(offset);
-        log!("{:#x}: {:2x}", offset, value_u8);
     }
     */
 
