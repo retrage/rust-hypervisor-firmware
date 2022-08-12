e0ed68ae29f011a73fb2a7967a0f31458f71c206
Akira Moroo <retrage01@gmail.com>
2022-08-12 15:37:06 +0900

    [WIP] main: Remove unused struct `FtdHeader`
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -196,21 +196,6 @@ pub extern "C" fn rust64_start() -> ! {
     main()
 }
 
-#[derive(Debug)]
-#[repr(C)]
-struct FtdHeader {
-    magic: u32,
-    totalsize: u32,
-    off_dt_struct: u32,
-    off_dt_strings: u32,
-    off_mem_rsvmap: u32,
-    version: u32,
-    last_comp_version: u32,
-    boot_cpuid_phys: u32,
-    size_dt_strings: u32,
-    size_dt_struct: u32,
-}
-
 /*
 const MAX_DEVICES: u8 = 32;
 const MAX_FUNCTIONS: u8 = 8;
