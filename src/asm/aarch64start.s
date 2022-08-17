1eece145a9d5a6d1998ea12b6d0b8ecc805a507f
Akira Moroo <retrage01@gmail.com>
2022-08-17 17:24:18 +0900

    asm: Remove redundant initialization
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/asm/aarch64start.s
+++ src/asm/aarch64start.s
@@ -5,12 +5,8 @@
 /* REF: https://fuchsia.googlesource.com/fuchsia/+/refs/heads/main/zircon/kernel/target/arm64/boot-shim/boot-shim.S */
 
 .global aarch64_header
-.global aarch64_start
 .extern stack_start
 .section .text.boot, "ax"
-
-/* scratch register, not saved across function calls */
-tmp .req x16
 
 aarch64_header:
   /* magic instruction that gives us UEFI "MZ" signature */
@@ -34,34 +30,9 @@ aarch64_header:
 aarch64_start:
   /* x0 typically points to device tree at entry */
 
-  /* what EL are we running at? */
-  mrs tmp, CurrentEL
-  cmp tmp, #(1 << 2)
-  beq cache_disable_el1
-
-  /* Disable caches and MMU (EL2 version) */
-  mrs tmp, sctlr_el2
-  bic tmp, tmp, #(1 << 12)  /* Instruction cache enable */
-  bic tmp, tmp, #(1 << 2)   /* Cache enable */
-  bic tmp, tmp, #(1 << 0)   /* MMU enable */
-  msr sctlr_el2, tmp
-  b cache_disable_done
-
-cache_disable_el1:
-  /* Disable caches and MMU (EL1 version) */
-  mrs tmp, sctlr_el1
-  bic tmp, tmp, #(1 << 12)  /* Instruction cache enable */
-  bic tmp, tmp, #(1 << 2)   /* Cache enable */
-  bic tmp, tmp, #(1 << 0)   /* MMU enable */
-  msr sctlr_el1, tmp
-
-cache_disable_done:
-  dsb sy
-  isb
-
   /* setup stack */
-  ldr tmp, =stack_start
-  mov sp, tmp
+  ldr x30, =stack_start
+  mov sp, x30
 
   /* x0: pointer to device tree */
   b rust64_start
