1d386b568e579343d8db622db04ad2c29e91fe8d
Akira Moroo <retrage01@gmail.com>
2022-09-28 09:15:41 +0000

    efi: Enable default secure boot related EFI variables
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/efi/mod.rs
+++ src/efi/mod.rs
@@ -1457,7 +1457,6 @@ pub fn efi_exec(
     }
     */
 
-    /*
     {
         let mut var_name = [0_u16; 64];
         crate::common::ascii_to_ucs2("SecureBoot", &mut var_name);
@@ -1493,7 +1492,6 @@ pub fn efi_exec(
             8,
             &mut data as *mut _ as *mut c_void);
     }
-    */
 
     ALLOCATOR.borrow_mut().dump_allocations();
 
