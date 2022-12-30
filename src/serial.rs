124badc33feff2b6191aa62a137c1fefc314d032
Akira Moroo <retrage01@gmail.com>
2022-12-30 16:37:33 +0900

    [Do Not Merge] serial: Add `dbg` macro from std
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- src/serial.rs
+++ src/serial.rs
@@ -50,3 +50,28 @@ macro_rules! log {
         println!($($arg)*);
     }};
 }
+
+#[macro_export]
+macro_rules! dbg {
+    // NOTE: We cannot use `concat!` to make a static string as a format argument
+    // of `eprintln!` because `file!` could contain a `{` or
+    // `$val` expression could be a block (`{ .. }`), in which case the `eprintln!`
+    // will be malformed.
+    () => {
+        $crate::log!("[{}:{}]", core::file!(), core::line!())
+    };
+    ($val:expr $(,)?) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                $crate::log!("[{}:{}] {} = {:#?}",
+                    core::file!(), core::line!(), core::stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
